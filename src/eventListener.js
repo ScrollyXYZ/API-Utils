3ab34bf1fb15997207455a1cbb9acc4cd227d520
Scrolly The Map <156605610+ScrollyXYZ@users.noreply.github.com>
2024-05-21 11:07:12 +0200

    downgread eth

--- src/eventListener.js
+++ src/eventListener.js
@@ -523,14 +523,14 @@ const ABI = [    {
     "stateMutability": "nonpayable",
     "type": "function"
   }];
-const provider = new ethers.providers.JsonRpcProvider('https://scroll.drpc.org');
-const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
-
-contract.on("Transfer", async (from, to, tokenId) => {
-  try {
-    await Token.findOneAndUpdate({ tokenId }, { owner: to.toLowerCase() }, { upsert: true });
-    console.log(`Token ${tokenId} ownership updated to ${to}`);
-  } catch (error) {
-    console.error("Error updating token ownership:", error);
-  }
-});
+  const provider = new ethers.providers.JsonRpcProvider('https://scroll.drpc.org');
+  const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+  
+  contract.on("Transfer", async (from, to, tokenId) => {
+    try {
+      await Token.findOneAndUpdate({ tokenId }, { owner: to.toLowerCase() }, { upsert: true });
+      console.log(`Token ${tokenId} ownership updated to ${to}`);
+    } catch (error) {
+      console.error("Error updating token ownership:", error);
+    }
+  });
