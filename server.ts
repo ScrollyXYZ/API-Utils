20bb5f115bc5b08385f6887ed5642aa4b8c671fe
Scrolly The Map <156605610+ScrollyXYZ@users.noreply.github.com>
2024-05-21 19:22:43 +0200

    router trigger cache

--- server.ts
+++ server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import './src/config/database'; // Ensure the database connection is initialized
-import { buildCache, monitorIdCounter } from './src/cacheBuilder';
-import './src/eventListener';
-import routes from './src/routes/index';
+import routes from './src/routes/index'; // Corrected the import path for routes
 
 dotenv.config();
 
@@ -15,13 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
-app.get('/trigger-cache', async (req, res) => {
-  await buildCache();
-  res.send('Cache build process triggered.');
-});
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-  buildCache(); // Build the initial cache when the server starts
-  monitorIdCounter(); // Start monitoring idCounter for new tokens
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import Token from '../models/token';
+import { buildCache } from '../cacheBuilder';
 
 const router = express.Router();
 
@@ -14,13 +15,23 @@ router.get('/tokens', async (req, res) => {
 });
 
 router.get('/database-status', async (req, res) => {
-    try {
-      const totalTokens = await Token.countDocuments();
-      res.json({ totalTokens });
-    } catch (error) {
-      console.error('Error fetching database status:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  try {
+    const totalTokens = await Token.countDocuments();
+    res.json({ totalTokens });
+  } catch (error) {
+    console.error('Error fetching database status:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/trigger-cache', async (req, res) => {
+  try {
+    await buildCache();
+    res.send('Cache build process triggered.');
+  } catch (error) {
+    console.error('Error triggering cache build:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 export default router;
