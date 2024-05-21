e9efa993d04fda7d32a17134cc5d1a6c16f1db67
Scrolly The Map <156605610+ScrollyXYZ@users.noreply.github.com>
2024-05-21 13:16:25 +0200

    ENV Contract-Address

--- src/cacheBuilder.ts
+++ src/cacheBuilder.ts
@@ -2,7 +2,7 @@ import { ethers } from 'ethers';
 import Token from './models/token';
 import { abi } from './config/abi';
 
-const CONTRACT_ADDRESS = "0xc2C543D39426bfd1dB66bBde2Dd9E4a5c7212876";
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
 
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,42 @@
 import express from 'express';
 import cors from 'cors';
-import './config/database';
-import router from './routes';
-import './cacheBuilder';
-import './eventListener';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import { buildCache } from './src/cacheBuilder';
+import './src/eventListener';
+import routes from './src/routes/index';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', router);
+
+app.use('/api', routes);
+
+mongoose.set('strictQuery', true);
+const mongoDBConnectionString = process.env.MONGODB_CONNECTION_STRING;
+
+if (!mongoDBConnectionString) {
+  console.error("MongoDB connection string is not defined in environment variables.");
+  process.exit(1);
+}
+
+mongoose.connect(mongoDBConnectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  buildCache(); // Build the initial cache when the server starts
 });
