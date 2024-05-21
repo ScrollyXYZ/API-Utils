05c7723111b4e809ecfc1465cd7269e99f4c9882
Scrolly The Map <156605610+ScrollyXYZ@users.noreply.github.com>
2024-05-21 10:35:00 +0200

    retry

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/nftDatabase', {
+mongoose.connect('your_mongodb_connection_string', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
