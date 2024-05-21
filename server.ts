d19eb56d11c25ccc6e5bb83cbae38d65760678c5
Scrolly The Map <156605610+ScrollyXYZ@users.noreply.github.com>
2024-05-21 13:27:54 +0200

    retry

--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import './src/config/database';
 import { buildCache } from './src/cacheBuilder';
 import './src/eventListener';
 import routes from './src/routes/index';
@@ -14,27 +13,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/api', routes);
-
-mongoose.set('strictQuery', true);
-const mongoDBConnectionString = process.env.MONGODB_CONNECTION_STRING;
-
-if (!mongoDBConnectionString) {
-  console.error("MongoDB connection string is not defined in environment variables.");
-  process.exit(1);
-}
-
-mongoose.connect(mongoDBConnectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
